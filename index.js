1e798f3b21f783923c39c4147af366c314215248
Joseph Abreau <joseph.abreau@gmail.com>
2023-09-25 15:49:15 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function writeCards(names, birthday) {
+    const cards = [];
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${birthday} gift!`);
+  }
+  return cards;
+}
+
+writeCards(["John", "Bob", "Wilbur"], "birthday");
+console.log(cards);
+
+function countDown(number) {
+    let num = number;
+    while (num >= 0) {
+        console.log(num--);
+    }
+}
+
+countDown(10);
